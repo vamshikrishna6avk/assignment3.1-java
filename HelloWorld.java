2d53de03cde08fecdf13f9a0806c88d8197a793f
vamshikrishna6avk <vamshikrishna6avk@gmail.com>
2017-11-03 16:04:43 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*program for printing hello world*/
+
+package session3;// here I have created a package as session3
+public class HelloWorld {
+	//declares a class called HelloWorld
+	//classes are the basics of opps(object oriented programming)
+	public static void main(String[] args){
+
+		System.out.println("hello world");
+		//Println is used to print text hello world and gives output
+	}
+
+}
